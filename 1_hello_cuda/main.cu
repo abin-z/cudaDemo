2114e9937cebf9fb52eaa07c0e1467e7f82fd76c
abin <abin.z@outlook.com>
2025-06-12 00:23:52 +0800

    style: format code

--- 1_hello_cuda/main.cu
+++ 1_hello_cuda/main.cu
@@ -2,13 +2,15 @@
 #include <iostream>
 
 // CUDA 核函数, 运行在 GPU 上
-__global__ void hello_cuda() {
-    printf("Hello from GPU!\n");
+__global__ void hello_cuda()
+{
+  printf("Hello from GPU!\n");
 }
 
-int main() {
-    hello_cuda<<<2, 2>>>();
-    cudaDeviceSynchronize();
-    std::cout << "Hello from CPU!" << std::endl;
-    return 0;
+int main()
+{
+  hello_cuda<<<2, 2>>>();
+  cudaDeviceSynchronize();
+  std::cout << "Hello from CPU!" << std::endl;
+  return 0;
 }
