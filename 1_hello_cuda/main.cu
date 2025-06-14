c99beec3d23c478a9f45a16bd67b7ae21ed3f551
abin <abin.z@outlook.com>
2025-06-15 00:23:59 +0800

    sytle: add comment

--- 1_hello_cuda/main.cu
+++ 1_hello_cuda/main.cu
@@ -1,13 +1,15 @@
 // main.cu
 #include <iostream>
 
-// CUDA 核函数, 运行在 GPU 上
+// 使用 __global__ 关键字声明
+// CUDA 核函数, 运行在 GPU 上, 可以从 CPU 调用 
+// 需要使用 <<<...>>> 语法来指定执行配置
 __global__ void hello_cuda()
 {
-  printf("Hello from GPU!\n");
+  printf("Hello from GPU!\n"); // 不能使用 std::cout, 因为它在 GPU 上运行
 }
 
-int main()
+int main(int argc, char** argv)
 {
   hello_cuda<<<1, 2>>>();
   cudaDeviceSynchronize();
