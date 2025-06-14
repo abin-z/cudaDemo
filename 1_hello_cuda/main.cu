172250d14a05cafce3babd36207b2b32e32db8aa
abin <abin.z@outlook.com>
2025-06-15 01:09:54 +0800

    feat: 开启 compile_commands.json 生成

--- 1_hello_cuda/main.cu
+++ 1_hello_cuda/main.cu
@@ -2,17 +2,17 @@
 #include <iostream>
 
 // 使用 __global__ 关键字声明
-// CUDA 核函数, 运行在 GPU 上, 可以从 CPU 调用 
+// CUDA 核函数, 运行在 GPU 上, 可以从 CPU 调用
 // 需要使用 <<<...>>> 语法来指定执行配置
 __global__ void hello_cuda()
 {
-  printf("Hello from GPU!\n"); // 不能使用 std::cout, 因为它在 GPU 上运行
+  printf("Hello from GPU!\n");  // 不能使用 std::cout, 因为它在 GPU 上运行
 }
 
 int main(int argc, char** argv)
 {
   hello_cuda<<<1, 2>>>();
-  cudaDeviceSynchronize(); // 同步函数, 等待 GPU 完成
+  cudaDeviceSynchronize();  // 同步函数, 等待 GPU 完成
   std::cout << "Hello from CPU!" << std::endl;
   return 0;
 }
--- 2_thread_model/main.cu
+++ 2_thread_model/main.cu
@@ -3,12 +3,12 @@
 // CUDA 核函数，运行在 GPU 上，可以从 CPU 调用
 __global__ void thread_model(int x)
 {
-  const int gx = gridDim.x; // 网格大小
-  const int bx = blockDim.x; // 块大小
+  const int gx = gridDim.x;   // 网格大小
+  const int bx = blockDim.x;  // 块大小
 
-  const int bid = blockIdx.x; // 块索引
-  const int tid = threadIdx.x; // 线程索引
-  const int gtid = bid * bx + tid; // 全局线程索引
+  const int bid = blockIdx.x;       // 块索引
+  const int tid = threadIdx.x;      // 线程索引
+  const int gtid = bid * bx + tid;  // 全局线程索引
   printf("Hello from GPU! Thread ID: %d, Block ID: %d, Global Thread ID: %d\n", tid, bid, gtid);
   printf("Hello from GPU! Grid size: %d, Block size: %d\n", gx, bx);
 }
